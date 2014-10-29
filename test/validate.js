88b050093b95112f6fbd55ae40b96aff2d971bf0
Nathan LaFreniere <quitlahok@gmail.com>
2014-10-29 12:09:22 -0700

    start testing validate script

---
+++ test/validate.js
@@ -0,0 +1,29 @@
+var Spawn = require('child_process').spawn;
+var Path = require('path');
+var Writable = require('stream').Writable;
+
+var Code = require('code');
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var expect = Code.expect;
+var describe = lab.experiment;
+var it = lab.test;
+
+describe('validate', function () {
+
+    it('runs', function (done) {
+
+        var validate = Spawn(Path.resolve(__dirname, '..', 'bin', 'validate'), [], { stdio: 'pipe' });
+        validate.stdout.on('data', function (chunk) {
+
+            chunk = chunk.toString();
+            expect(chunk).to.equal('Running validate checks...\n');
+        });
+
+        validate.on('close', function (code) {
+
+            expect(code).to.equal(0);
+            done();
+        });
+    });
+});
