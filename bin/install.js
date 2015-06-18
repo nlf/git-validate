c7010b680f1ce16b8315f86b3d41cb2b4adf01b9
Nathan LaFreniere <quitlahok@gmail.com>
2015-06-18 09:40:01 -0700

    cleanup install script

--- bin/install.js
+++ bin/install.js
@@ -1,14 +1,3 @@
-var Fs = require('fs');
-var Path = require('path');
 var Utils = require('../lib/utils');
 
-var projectRoot;
-try {
-    projectRoot = Utils.findProjectRoot();
-}
-catch (e) {
-    process.exit();
-}
-
-var project = JSON.parse(Fs.readFileSync(Path.join(projectRoot, 'package.json'), 'utf8'));
 Utils.installHooks(['pre-commit', 'pre-push']);
