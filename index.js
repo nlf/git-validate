582b38a52b6740b0f395c3b1fc425fc274bce3a2
Nathan LaFreniere <quitlahok@gmail.com>
2015-06-19 14:04:10 -0700

    add missing exports

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ var Utils = require('./lib/utils');
 
 exports.copy = Utils.copy;
 exports.installHooks = Utils.installHooks;
+exports.installScript = Utils.installScript;
+exports.configureHook = Utils.configureHook;
