94069d8d5f259eabc497dc2953ad9d159e948e79
Nathan LaFreniere <quitlahok@gmail.com>
2015-09-18 10:40:02 -0700

    use \r instead of literal carriage return, for #34

--- bin/validate.sh
+++ bin/validate.sh
@@ -44,8 +44,7 @@ tokenize() {
     local KEYWORD='null|false|true'
     local SPACE='[[:space:]]+'
     
-    sed 's/
-$//' | $GREP "$STRING|$NUMBER|$KEYWORD|$SPACE|." | egrep -v "^$SPACE$"
+    sed 's/\r//' | $GREP "$STRING|$NUMBER|$KEYWORD|$SPACE|." | egrep -v "^$SPACE$"
 }
 
 parse_array() {
